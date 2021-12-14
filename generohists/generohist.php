ecfeb0cb8616d125a704531c4606af65e2fc36e9
01737 <59713688+LelioMarcos@users.noreply.github.com>
2021-12-14 16:36:10 -0300

    create and delete generohist

--- generohists/generohist.php
+++ generohists/generohist.php
@@ -38,79 +38,49 @@ class GeneroHist {
 		
 		return $stmt;
 	}
-	// public function create(){
-	// 	$query = 'INSERT into ' . $this->table . ' (dsccorpocoment, idusuario, idhist)
-	// 	values(:dsccorpocoment, :idusuario, :idhist)';
-		
-	// 	//prepare statement
-	// 	$stmt = $this->conn->prepare($query);
-		
-	// 	$this->comment = htmlspecialchars(strip_tags($this->comment));
 
-	// 	//binding of parameters
-	// 	$stmt->bindParam(':dsccorpocoment', $this->comment);
-	// 	$stmt->bindParam(':idusuario', $this->idusuario);
-	// 	$stmt->bindParam(':idhist', $this->idhist);
+	public function create() {
+		$query = 'INSERT into ' . $this->table . ' (fk_historia_idhist, fk_genero_idgenero)
+		values (:fk_historia_idhist, :fk_genero_idgenero)';
 		
-	// 	//execute the query
-	// 	if($stmt->execute()){
-	// 		return true;
-	// 	}
+		//prepare statement
+		$stmt = $this->conn->prepare($query);
 		
-	// 	//print erro if something goes wrong
-	// 	printf("Error %s. \n", $stmt->error);
+		//binding of parameters
+		$stmt->bindParam(':fk_historia_idhist', $this->idhist);
+		$stmt->bindParam(':fk_genero_idgenero', $this->idgenero);
 		
-	// 	return false;
-	// }
-	
-    
-	// public function update(){
-	// 	$query = 'UPDATE '. $this->table . ' SET dsccorpocoment = :dsccorpocoment WHERE id = :id';
-	
-	// 	//prepare statement
-	// 	$stmt = $this->conn->prepare($query);
-	// 	//clean data
-	// 	$this->comment = htmlspecialchars(strip_tags($this->comment));
-	// 	$this->id = htmlspecialchars(strip_tags($this->id));
-	
-	// 	//binding of parameters
-	// 	$stmt->bindParam(':dsccorpocoment', $this->comment);
-	// 	$stmt->bindParam(':idcoment', $this->id);
-	
-	// 	//execute the query
-	// 	if($stmt->execute()){
-	// 		return true;
+		//execute the query
+		if($stmt->execute()){
+			return true;
+		}
 		
-	// 	}
-	
-	// 	//print erro if something goes wrong
-	// 	printf("Error %s. \n", $stmt->error);
-	
-	// 	return false;
-	// }
-	
-	
-	// public function delete(){
-	// 	$query = 'DELETE FROM '. $this->table . ' WHERE idcoment = :id';
+		//print erro if something goes wrong
+		printf("Error %s. \n", $stmt->error);
 		
-	// 	//prepare statement
-	// 	$stmt = $this->conn->prepare($query);
-	// 	//clean data
-	// 	$this->id = htmlspecialchars(strip_tags($this->id));
+		return false;
+	}
+	
+	public function delete(){
+		$query = 'DELETE FROM '. $this->table . ' WHERE idgenerohist = :id';
 		
-	// 	//binding of parameters
-	// 	$stmt->bindParam(':id', $this->id);
+		//prepare statement
+		$stmt = $this->conn->prepare($query);
+		//clean data
+		$this->id = htmlspecialchars(strip_tags($this->id));
 		
-	// 	//execute the query
-	// 	if($stmt->execute()){
-	// 		return true;
-			
-	// 	}
-	// 	//print erro if something goes wrong
-	// 	printf("Error %s. \n", $stmt->error);
+		//binding of parameters
+		$stmt->bindParam(':id', $this->id);
 		
-	// 	return false;
-	// }
+		//execute the query
+		if($stmt->execute()){
+			return true;
+		}
+		//print erro if something goes wrong
+		printf("Error %s. \n", $stmt->error);
+		
+		return false;
+	}
 }
 
 ?>
