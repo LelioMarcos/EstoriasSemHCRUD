811a368089c2c3c82e83c0e6fd08d4db99b04208
01737 <59713688+LelioMarcos@users.noreply.github.com>
2021-12-17 13:42:25 -0300

    qweqwe

--- users/create_user.php
+++ users/create_user.php
@@ -2,14 +2,6 @@
 //headers - comando que especifica características da resposta do cabeçalho HTTP.
 
 //Domínios autorizados a acessar os recursos do servidor
-header('Access-Control-Allow-Origin: *');
-//Indica que o formato do corpo da solicitação é JSON
-header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: POST');
-
-//Especificando os headers permitindos na requisição
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
-
 //incializa banco de dados e método POST
 include_once('../initialize.php'); 
 
