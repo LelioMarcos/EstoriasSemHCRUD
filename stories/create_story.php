be0e9b40779224fcadfbd63beffa14a1ed9e2331
01737 <59713688+LelioMarcos@users.noreply.github.com>
2021-12-17 12:51:10 -0300

    asdasd

--- stories/create_story.php
+++ stories/create_story.php
@@ -16,19 +16,26 @@ include_once('../initialize.php');
 //Instancia objeto story com a conexão com o banco de dados
 $story = new Story($db);
 
-$story->titulo = isset($_POST['titulo']) ? $_POST['titulo'] : die();
-$story->sinopse = isset($_POST['sinopse']) ? $_POST['sinopse'] : die();
-$story->corpo = isset($_POST['corpo']) ? $_POST['corpo'] : die();
-$story->idusuario = isset($_POST['idusuario']) ? $_POST['idusuario'] : die();
-$story->idcapa = isset($_POST['idcapa']) ? $_POST['idcapa'] : die();
+if (isset($_POST['titulo']) && isset($_POST['sinopse']) && isset($_POST['corpo']) && isset($_POST['idusuario']) && isset($_POST['idcapa'])) {
+	$story->titulo = isset($_POST['titulo']) ? $_POST['titulo'] : die();
+	$story->sinopse = isset($_POST['sinopse']) ? $_POST['sinopse'] : die();
+	$story->corpo = isset($_POST['corpo']) ? $_POST['corpo'] : die();
+	$story->idusuario = isset($_POST['idusuario']) ? $_POST['idusuario'] : die();
+	$story->idcapa = isset($_POST['idcapa']) ? $_POST['idcapa'] : die();
 
-//Chamada ao método create
-if($story->create()){
-	header(http_response_code(201));
-	echo json_encode(
-		array('message' => 'Story created.')
-	);
-}else{
+	//Chamada ao método create
+	if($story->create()){
+		header(http_response_code(201));
+		echo json_encode(
+			array('message' => 'Story created.')
+		);
+	}else{
+		header(http_response_code(500));
+		echo json_encode(
+			array('message' => 'Story not created.')
+		);
+	}
+} else {
 	header(http_response_code(500));
 	echo json_encode(
 		array('message' => 'Story not created.')
