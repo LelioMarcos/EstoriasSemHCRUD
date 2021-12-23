4c14d1a3c65623c6db6c65e6d3544c60fd019862
01737 <59713688+LelioMarcos@users.noreply.github.com>
2021-12-23 00:12:00 -0300

    genero hist

--- generohists/generohist.php
+++ generohists/generohist.php
@@ -42,14 +42,14 @@ class GeneroHist {
 
 	public function create() {
 		$query = 'INSERT into ' . $this->table . ' (fk_historia_idhist, fk_genero_idgenero)
-		values (:fk_historia_idhist, (SELECT idgenero FROM hsemh.genero WHERE dscgenero = :dscgenero))';
+		values (:fk_historia_idhist, :fk_genero_idgenero)';
 		
 		//prepare statement
 		$stmt = $this->conn->prepare($query);
 		
 		//binding of parameters
 		$stmt->bindParam(':fk_historia_idhist', $this->idhist);
-		$stmt->bindParam(':dscgenero', $this->nomgenero);
+		$stmt->bindParam(':fk_genero_idgenero', $this->idgenero);
 		
 		//execute the query
 		if($stmt->execute()){
