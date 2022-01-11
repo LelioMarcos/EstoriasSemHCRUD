aab22e39b7ceb348aa35d30d7fd0559fc219eeb0
01737 <59713688+LelioMarcos@users.noreply.github.com>
2022-01-11 15:01:14 -0300

    index.php

--- index.php
+++ index.php
@@ -6,12 +6,10 @@
 	</head>
 	<body>
 		<h1>ESTÓRIAS SEM H!!</h1>
-		<h3>O CRUD mais bonito da rua</h3>
 
 		<ul>
 			<li><a href="stories/get_stories.php">Get Stories</a></li>
 			<li><a href="users/get_users.php">Get Users</a></li>
-			<li><a href="respostas/get_respostas.php">Get Respsostas</a></li>
 			<li><a href="genders/get_genders.php">Get Genders</a></li>
 			<li><a href="classificacoes/get_classificacoes.php">Get Classificações</a></li>
 			<li><a href="generohists/get_generohists.php">Get GeneroHists</a></li>
